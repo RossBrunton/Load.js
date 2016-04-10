f1ffd56796163eb6e1a147bfb91c9b9428d8bdaf
Ross Brunton <bruntonross@gmail.com>
2016-04-10 21:03:40 +0100

    generateDeps now allows relative directory
    So you can specify what the files you create will be relative to. Should
    usually be the folder you put deps.json in.

--- tools/generateDeps.py
+++ tools/generateDeps.py
@@ -2,8 +2,9 @@
 
 # Generates a dependancy file
 
-# > generateDeps.py [path] [obj] [indent]
+# > generateDeps.py path [rel] [obj] [indent]
 # Path is the path to (recursivley) generate dependancies for, defaults to the current directory
+# Rel is the path that the files will be relative to, this should be the directory that a "deps.json" is output
 # Obj will be merged with the dependancies, and any properties on it will be present in the exported file.
 # Indent is the amount to indent (an integer) for pretty printing,
 #  if not specified then the file will be compressed as much as possible
@@ -27,24 +28,28 @@ reqRPatt = re.compile(r"load\.requireResource\(\s*"+string, re.MULTILINE | re.DO
 reqEPatt = re.compile(r"load\.requireExternal\(\s*"+string+comma+string, re.MULTILINE | re.DOTALL)
 reqEAPatt = re.compile(r"load\.requireExternal\(\s*?"+string+comma+string+comma+array, re.MULTILINE | re.DOTALL)
 
-provPatt = re.compile(r"load\.provide\(\s*\"([^\"]+?)\"", re.MULTILINE | re.DOTALL)
+provPatt = re.compile(r"load\.provide\(\s*"+string, re.MULTILINE | re.DOTALL)
 provRPatt = re.compile(r"load\.provideResource\(\s*\"([^\"]+?)\"", re.MULTILINE | re.DOTALL)
 data = []
 
-if len(sys.argv) > 1:
-	os.chdir(sys.argv[1])
+rel = sys.argv[1]
+if len(sys.argv) >= 3:
+	rel = sys.argv[2]
+
+def handlePath(f):
+	return posixpath.relpath(f, rel)
 
 def addPack(path, size, type):
-	obj = [path, [], [], size, type]
+	obj = [handlePath(path), [], [], size, type]
 	data.append(obj)
 	
 	return obj;
 
-for root, dirs, files in os.walk("."):
+for root, dirs, files in os.walk(sys.argv[1]):
 	for f in files:
 		if f[-3:] == ".js":
 			with open(os.path.join(root, f)) as reader:
-				pack = addPack(posixpath.join(root, f)[2:], os.path.getsize(os.path.join(root, f)), TYPE_PACK)
+				pack = addPack(posixpath.join(root, f), os.path.getsize(os.path.join(root, f)), TYPE_PACK)
 				
 				contents = reader.read()
 				# load.provide
@@ -88,10 +93,10 @@ for root, dirs, files in os.walk("."):
 				pack[2].sort()
 
 export = {"version":1, "packages":data}
-if len(sys.argv) >= 3:
-	export.update(json.loads(sys.argv[2]))
+if len(sys.argv) >= 4:
+	export.update(json.loads(sys.argv[3]))
 
-if len(sys.argv) > 3:
-	print json.dumps(export, indent=int(sys.argv[3]))
+if len(sys.argv) >= 5:
+	print json.dumps(export, indent=int(sys.argv[4]))
 else:
 	print json.dumps(export, separators=(',', ':'))
