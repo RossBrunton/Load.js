f6477d1d92c567f02366b3955f530ccf79bd09d7
Ross Brunton <bruntonross@gmail.com>
2016-04-11 11:38:15 +0100

    Tabs to spaces
    Spaces are probably better

--- tools/generateDeps.py
+++ tools/generateDeps.py
@@ -34,75 +34,75 @@ data = []
 
 rel = sys.argv[1]
 if len(sys.argv) >= 3:
-	rel = sys.argv[2]
+    rel = sys.argv[2]
 
 def handlePath(f):
-	# Check if URL
-	if f.startswith("//") or f.startswith("http://") or f.startswith("https://"):
-		return f
-	
-	return posixpath.relpath(f, rel)
+    # Check if URL
+    if f.startswith("//") or f.startswith("http://") or f.startswith("https://"):
+        return f
+    
+    return posixpath.relpath(f, rel)
 
 def addPack(path, size, type):
-	obj = [handlePath(path), [], [], size, type]
-	data.append(obj)
-	
-	return obj;
+    obj = [handlePath(path), [], [], size, type]
+    data.append(obj)
+    
+    return obj;
 
 for root, dirs, files in os.walk(sys.argv[1]):
-	for f in files:
-		if f[-3:] == ".js":
-			with open(os.path.join(root, f)) as reader:
-				pack = addPack(posixpath.join(root, f), os.path.getsize(os.path.join(root, f)), TYPE_PACK)
-				
-				contents = reader.read()
-				# load.provide
-				for match in provPatt.finditer(contents):
-					if match.group(1) not in pack[1]:
-						pack[1].append(match.group(1))
-				
-				# load.provideResaurce
-				for match in provRPatt.finditer(contents):
-					if match.group(1) not in pack[1]:
-						pack[1].append(match.group(1))
-				
-				# load.require
-				for match in reqPatt.finditer(contents):
-					if match.group(1) not in pack[2]:
-						pack[2].append(match.group(1))
-				
-				# load.requireResource
-				for match in reqRPatt.finditer(contents):
-					if match.group(1) not in pack[2]:
-						location = posixpath.join(root, match.group(1))
-						
-						res = addPack(location, os.path.getsize(os.path.join(root, match.group(1))), TYPE_RES)
-						res[1] = [match.group(1)]
-						pack[2].append(match.group(1))
-				
-				# load.requireExternal (arg)
-				for match in reqEAPatt.finditer(contents):
-					if match.group(1) not in pack[2]:
-						res = addPack(match.group(2), 0, TYPE_EXT)
-						res[1] = [match.group(1)]
-						res[2] = json.loads(match.group(3))
-						pack[2].append(match.group(1))
-					
-				# load.requireExternal (no arg)
-				for match in reqEPatt.finditer(contents):
-					if match.group(1) not in pack[2]:
-						res = addPack(match.group(2), 0, TYPE_EXT)
-						res[1] = [match.group(1)]
-						pack[2].append(match.group(1))
-				
-				pack[1].sort()
-				pack[2].sort()
+    for f in files:
+        if f[-3:] == ".js":
+            with open(os.path.join(root, f)) as reader:
+                pack = addPack(posixpath.join(root, f), os.path.getsize(os.path.join(root, f)), TYPE_PACK)
+                
+                contents = reader.read()
+                # load.provide
+                for match in provPatt.finditer(contents):
+                    if match.group(1) not in pack[1]:
+                        pack[1].append(match.group(1))
+                
+                # load.provideResaurce
+                for match in provRPatt.finditer(contents):
+                    if match.group(1) not in pack[1]:
+                        pack[1].append(match.group(1))
+                
+                # load.require
+                for match in reqPatt.finditer(contents):
+                    if match.group(1) not in pack[2]:
+                        pack[2].append(match.group(1))
+                
+                # load.requireResource
+                for match in reqRPatt.finditer(contents):
+                    if match.group(1) not in pack[2]:
+                        location = posixpath.join(root, match.group(1))
+                        
+                        res = addPack(location, os.path.getsize(os.path.join(root, match.group(1))), TYPE_RES)
+                        res[1] = [match.group(1)]
+                        pack[2].append(match.group(1))
+                
+                # load.requireExternal (arg)
+                for match in reqEAPatt.finditer(contents):
+                    if match.group(1) not in pack[2]:
+                        res = addPack(match.group(2), 0, TYPE_EXT)
+                        res[1] = [match.group(1)]
+                        res[2] = json.loads(match.group(3))
+                        pack[2].append(match.group(1))
+                    
+                # load.requireExternal (no arg)
+                for match in reqEPatt.finditer(contents):
+                    if match.group(1) not in pack[2]:
+                        res = addPack(match.group(2), 0, TYPE_EXT)
+                        res[1] = [match.group(1)]
+                        pack[2].append(match.group(1))
+                
+                pack[1].sort()
+                pack[2].sort()
 
 export = {"version":1, "packages":data}
 if len(sys.argv) >= 4:
-	export.update(json.loads(sys.argv[3]))
+    export.update(json.loads(sys.argv[3]))
 
 if len(sys.argv) >= 5:
-	print json.dumps(export, indent=int(sys.argv[4]))
+    print json.dumps(export, indent=int(sys.argv[4]))
 else:
-	print json.dumps(export, separators=(',', ':'))
+    print json.dumps(export, separators=(',', ':'))
